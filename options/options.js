61d87c486c6d1890e76378278a2d35bf92212ecd
Crowdbotics <engineering@crowdbotics.com>
2023-03-12 06:42:22 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -5,8 +5,10 @@ export const globalOptions = {
   url: "https://tst-agsdjh-dev-68558.botics.co",
   api: "https://tst-agsdjh-dev-68558.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
-  }
+  },
+  undefined: { title: "App Menu", copy: "Routes available!", styles: header }
 }
